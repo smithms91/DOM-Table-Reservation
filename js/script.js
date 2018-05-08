3e1b51815746a9080bb33ef3103c314b32f03ef0
Michael Smith <smithms91@gmail.com>
2018-05-08 19:05:58 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,34 @@
 "use strict";
 
 $(document).ready(() => {
-    
     //Initially hiding the box
     $("#reserve_box").hide();
-
-    //Tables that are available to click
-    $(document).on("click", "available", () => {
-        $("#reserve_box").slideToggle();
-        console.log("YES YOU CLICKED AVAILABLE");
-    });
-
-    //Clicking the X to close the form
-    $(".location").on("click", () => {
-        $("#reserve_box").slideToggle();
-    });
-
-    //Submit Button
-    $("#button").on("click", () => {
-        let username = $("#name").val();
-        console.log(username);
-        $("input").val("");
-        $("#reserve_box").slideToggle();
-    });
-
-    //Close form on escape key
-    $(document).on('keydown', (e) => {
-        if (e.keyCode === 27) { // ESC
-            $($("#reserve_box")).fadeOut();
-        }
-    });
-
+    //When I click anywhere on the document with a class of available
     $(document).on("click", ".available", (event) => {
-        $(event.target).removeClass("available").addClass("reserved");
+        //Click X Button to close
+        $(".location").on("click", () => {
+            $("#reserve_box").fadeOut();
+        });
+        //Escape to close
+        $(document).on('keydown', (e) => {
+            if (e.keyCode === 27) { // ESC
+                $("#reserve_box").fadeOut();
+            }
+        });
+        //Click submit do stuff
+        $("#button").on("click", () => {    
+            $(event.target).removeClass("available").addClass("reserved");
+            $("#reserve_box").slideUp();
+            let name = $("#name").val();
+            let number = $("#number").val();
+            let quantity = $("#quantity").val();
+            console.log(name.val());
+            console.log(number.val());
+            console.log(quantity.val());
+            name.val("");
+            number.val("");
+            quantity.val("");
+        });
         $("#reserve_box").slideToggle();
     })
-
 });
